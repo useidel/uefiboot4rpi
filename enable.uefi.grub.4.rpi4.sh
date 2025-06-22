e951731811639c3e77f536308302b8563ea99b9b
Udo Seidel <udoseidel@gmx.de>
2025-06-22 15:02:43 +0200

    some small improvements to the script

--- enable.uefi.grub.4.rpi4.sh
+++ enable.uefi.grub.4.rpi4.sh
@@ -2,6 +2,32 @@
 PATH=/bin:/sbin:/usr/sbin:/usr/sbin
 export PATH
 
+# Pseude devices which should not cause harm
+MYDEV=/dev/MYDEV
+MYPART1=`echo $MYDEV|sed 's/$/p1/g'`
+
+# Are we on Debian'ish
+which apt > /dev/null 
+if [ $? -ne 0 ]
+then
+	echo " We are very likely not on the right operating system"
+	echo " Better to bail out now"
+	exit 1
+fi
+
+# some more checks
+mount|grep 'boot/firmware' > /dev/null 
+if [ $? -ne 0 ]
+then
+	echo " Something is not right here"
+	echo " Safe exit here"
+	exit 1
+else
+	MYPART1=`mount|grep 'boot/firmware' |awk '{print $1}'`
+	MYDEV=`echo $MYPART| sed 's/p1$//'`
+fi
+
+
 # Getting up-to-date
 apt update && apt -y upgrade
 
@@ -12,7 +38,7 @@ apt -y install grub2-common grub-efi-arm64-bin grub-efi-arm64 grub-efi-arm64-sig
 # We will re-use the partition for the EFI setup
 umount /boot/firmware
 mkdir /tmp/mnt
-mount /dev/mmcblk0p1 /tmp/mnt
+mount $MYPART1 /tmp/mnt
 cd /tmp/mnt
 cp -a * /boot/firmware/
 sync
@@ -29,7 +55,7 @@ systemctl daemon-reload
 
 # Now changed the type of partition 1 to "uefi" and also mark it as bootable
 # the latter is probably not needed - but does not harm and could be handy for reinstallations
-fdisk /dev/mmcblk0 << EOF
+fdisk $MYDEV << EOF
 a
 1
 t
@@ -41,8 +67,8 @@ EOF
 # Tell the kernel to reload the new partition table .. just in case
 partprobe
 # create a filesystem on the EFI partition and mount it
-mkfs.fat -n EFI /dev/mmcblk0p1
-mount /dev/mmcblk0p1 /boot/efi
+mkfs.fat -n EFI $MYPART1
+mount $MYPART1 /boot/efi
 # actually a "mount -a" should now work as well
 
 # Get the UEFI firmeware files for the RPiv4 family
